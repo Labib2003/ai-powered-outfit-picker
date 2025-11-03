ce10a151c6cc6b04707c60ee6a6fce52b8b66fc2
Labib2003 <labib.salimi30@gmail.com>
2025-11-03 13:36:08 +0600

    fix: add image url column to both category and product table

--- db/schema/category.ts
+++ db/schema/category.ts
@@ -4,6 +4,7 @@ export const categories = pgTable("categories", {
   id: uuid().primaryKey(),
 
   name: varchar("name", { length: 255 }).notNull(),
+  imageUrl: varchar("image_url", { length: 255 }),
   description: varchar("description").notNull(),
 
   createdAt: timestamp("created_at").notNull().defaultNow(),
--- db/schema/product.ts
+++ db/schema/product.ts
@@ -15,6 +15,7 @@ export const products = pgTable("products", {
   id: uuid("id").primaryKey(),
 
   name: varchar("name", { length: 255 }).notNull(),
+  imageUrl: varchar("image_url", { length: 255 }),
   description: varchar("description").notNull(),
   availableSizes: sizesEnum("available_sizes").array().notNull(),
   price: numeric("price", { precision: 10, scale: 2 }).notNull(),
