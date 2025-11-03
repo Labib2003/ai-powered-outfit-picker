5af156fab088aa3e30097230cea3a59c059f271d
Labib2003 <labib.salimi30@gmail.com>
2025-11-03 13:09:03 +0600

    feat: design db schema for product and category including vector embedding support

---
+++ db/schema/category.ts
@@ -0,0 +1,13 @@
+import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
+
+export const categories = pgTable("categories", {
+  id: uuid().primaryKey(),
+
+  name: varchar("name", { length: 255 }).notNull(),
+  description: varchar("description").notNull(),
+
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .$onUpdateFn(() => new Date()),
+});
---
+++ db/schema/product.ts
@@ -0,0 +1,31 @@
+import {
+  numeric,
+  pgEnum,
+  pgTable,
+  timestamp,
+  uuid,
+  varchar,
+  vector,
+} from "drizzle-orm/pg-core";
+import { categories } from "./category";
+
+export const sizesEnum = pgEnum("sizes", ["S", "M", "L", "XL"]);
+
+export const products = pgTable("products", {
+  id: uuid("id").primaryKey(),
+
+  name: varchar("name", { length: 255 }).notNull(),
+  description: varchar("description").notNull(),
+  availableSizes: sizesEnum("available_sizes").array().notNull(),
+  price: numeric("price", { precision: 10, scale: 2 }).notNull(),
+  embedding: vector("embedding", { dimensions: 1536 }),
+
+  categoryId: uuid("category_id")
+    .references(() => categories.id)
+    .notNull(),
+
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .$onUpdateFn(() => new Date()),
+});
